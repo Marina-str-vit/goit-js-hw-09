09401a83dcfc123b0b031f4cb4b13702196179b8
Marina-str-vit <meribsv@gmail.com>
2024-11-16 22:39:44 +0000

    Deploying to gh-pages from @ Marina-str-vit/goit-js-hw-09@9d62dba6e1a08a305dc29246e5690e3f7c2fd962 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as a}from"./assets/vendor-CgTBfC_f.js";const i=document.querySelector(".gallery");i.insertAdjacentHTML("beforeend",o(images));function o(e){return e.map(({preview:l,original:r,description:t})=>`
+    <li class="gallery-item">
+        <a class="gallery-link" href="${r}">
+      <img
+        class="gallery-image"
+        src="${l}"
+        alt="${t}"
+        />
+      </a>
+    </li>
+    `).join("")}new a(".gallery a",{captionsData:"alt",captionDelay:250});console.log(a);
 //# sourceMappingURL=1-gallery.js.map
