9d62dba6e1a08a305dc29246e5690e3f7c2fd962
Marina-str-vit <meribsv@gmail.comy>
2024-11-17 00:39:23 +0200

    add galler js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,31 @@
-console.log("Gallery");
+// import arrGallery from ".."
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import "simplelightbox/dist/simple-lightbox.min.js"
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML("beforeend", createMarkup(images));
+
+function createMarkup(arrGallery) {
+  return arrGallery.map(({ preview, original, description }) => `
+    <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+      <img
+        class="gallery-image"
+        src="${preview}"
+        alt="${description}"
+        />
+      </a>
+    </li>
+    `).join("");      
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+console.log(SimpleLightbox);
